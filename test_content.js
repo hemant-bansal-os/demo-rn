63c463060c8cd866132292190ce75a66c0e43650
Ravikant Shahi <ravikant.shahi@glocoach.com>
2024-11-14 16:33:49 +0530

    new commit

---
+++ test_content.js
@@ -0,0 +1,23 @@
+conf_data.startInterviewMessage += ` ${language_json?.lance_warning["and Please keep the interview language"]} and I have taken this interview before.`;
+    // "Hi, I am Talent {{coachee_name}}":"Hi, I am Talent {{coachee_name}}",
+    // "and my gender is {{gender}}":" and my gender is {{gender}}",
+    // `Hi, I am Talent ${coachee_name} and my gender is ${gender}`;
+
+    conf_data.finishedMessage =  t(`screens.AiScreen.text.finished_message`),
+    // `Congratulations! Your interview with me on your key challenges and areas of improvement has concluded and I will now summarize it in a Personal Discovery Report.`;
+
+    conf_data.notFinishedMessage = t(`screens.AiScreen.text.unfinished_interview_warning`) + t(`screens.AiScreen.text.finish_interview_warning`);
+    // `Your Interview to discuss your focus areas and areas of improvement is not completed. I still have some questions for you.\n
+    // Are you sure you still want to finish the interview?`;
+
+
+    conf_data.notFinishedthalaforMessage = t(`screens.AiScreen.text.unfinished_interview_warning`) + t(`screens.AiScreen.text.finish_interview_warning`);
+    // `Your Interview to discuss your focus areas and areas of improvement is not completed. I still have some questions for you.\n
+    // Are you sure you still want to finish the interview?`;
+
+    conf_data.notFinishedthakaharaMessage = t(`screens.AiScreen.text.unfinished_interview_warning`) + t(`screens.AiScreen.text.finish_interview_warning`);
+    // `Your Interview to discuss your focus areas and areas of improvement is not completed. I still have some questions for you.\n
+    // Are you sure you still want to finish the interview?`;
+
+
+    hello data ihhh
